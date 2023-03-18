08a917e3802af2d75b8e7354448e912d8f4dffb5
Vasallo94 <65018705+Vasallo94@users.noreply.github.com>
2023-03-18 20:46:27 +0100

    Update humedad.py

--- humedad.py
+++ humedad.py
@@ -1,4 +1,4 @@
-# --------------------LIBRERÍAS----------------------------#
+# --------------------LIBRARIES----------------------------#
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,70 +8,46 @@ import plotly_express as px
 import plotly.graph_objects as go
 
 
-# --------------------CONFIGURACIÓN DE LA PÁGINA----------------------------#
+# --------------------PAGE CONFIGURATION----------------------------#
 # layout="centered" or "wide"
 st.set_page_config(page_title="Humedades", layout="wide", page_icon="💦")
 st.set_option("deprecation.showPyplotGlobalUse", False)
 
-st.title("Humedades")
-st.markdown(
-    "<center><h2><l style='color:white; font-size: 30px;'>Primeros acercamientos al problema de humedades que tengo en mi casa. En desarrollo...</h2></center>",
-    unsafe_allow_html=True,)
-# --------------------IMPORTACIÓN DE LOS DATAFRAME----------------------------#
-df_habitacion = pd.read_csv("data/Habitacion_export_202303131817.csv")
-df_salon = pd.read_csv("data/Salon_export_202303131824.csv")
+# --------------------LOAD DATAFRAMES----------------------------#
+# Load CSV files for temperature and humidity data from two sensors in the house
+try:
+    df_habitacion = pd.read_csv("data/Habitacion_export_202303131817.csv")
+    df_salon = pd.read_csv("data/Salon_export_202303131824.csv")
+except FileNotFoundError:
+    st.warning("No se encontraron los archivos CSV. Por favor, asegúrate de que estén en la carpeta 'data' y con los nombres correctos.")
+    st.stop()
 
-
-# Concatenar los dataframes en uno solo con Multiíndice
-df = pd.concat(
-    [df_habitacion, df_salon], keys=["habitación", "salón"], names=["ubicación"]
-)
+# Concatenate the dataframes with a multi-index
+df = pd.concat([df_habitacion, df_salon], keys=["Habitación", "Salón"], names=["Ubicación"])
 df = df.reorder_levels([1, 0]).sort_index()
-# opciones para la selección de ubicaciones
-ubicaciones_disponibles = ["habitación", "salón"]
-# valores predeterminados para la selección de ubicaciones
-ubicaciones_predeterminadas = st.sidebar.multiselect(
-    "Selecciona la ubicación", ubicaciones_disponibles
-)
-st.dataframe(df)
-# verificar que los valores predeterminados estén en la lista de opciones
-ubicaciones_validas = [
-    ubicacion
-    for ubicacion in ubicaciones_predeterminadas
-    if ubicacion in ubicaciones_disponibles
-]
 
-# # --------------------SIDEBAR----------------------------#
-# # st.sidebar.image("img/logo.png", width=150)
-# st.sidebar.title("MENÚ")
-# st.sidebar.subheader("")
-# st.sidebar.write("")
+# --------------------SIDEBAR----------------------------#
+# Display the sidebar for selecting locations
+ubicaciones_disponibles = ["Habitación", "Salón"]
+ubicaciones_predeterminadas = st.sidebar.multiselect("Selecciona la ubicación", ubicaciones_disponibles)
+ubicaciones_validas = [ubicacion for ubicacion in ubicaciones_predeterminadas if ubicacion in ubicaciones_disponibles]
 
-# mostrar la selección de ubicaciones válidas y plotear con la decisión
+# --------------------DATA VISUALIZATION----------------------------#
+# Show charts for selected locations
 if ubicaciones_validas:
-    df_seleccionado = df.loc[
-        df.index.get_level_values("ubicación").isin(ubicaciones_predeterminadas)
-    ]
-    fig1 = px.line(
-        x=df_seleccionado["Registro_temporal"],
-        y=df_seleccionado["Temperatura_Celsius"],
-    )
+    df_seleccionado = df.loc[df.index.get_level_values("Ubicación").isin(ubicaciones_predeterminadas)]
+
+    # Line chart for temperature over time
+    fig1 = px.line(x=df_seleccionado["Registro_temporal"], y=df_seleccionado["Temperatura_Celsius"])
     st.plotly_chart(fig1)
 
-    fig2 = px.line(
-        df_seleccionado,
-        x=df_seleccionado["Registro_temporal"],
-        y=df_seleccionado["Humedad_relativa[%]"],
-    )
+    # Line chart for humidity over time
+    fig2 = px.line(df_seleccionado, x=df_seleccionado["Registro_temporal"], y=df_seleccionado["Humedad_relativa[%]"])
     st.plotly_chart(fig2)
-    fig3 = px.line(
-        df_seleccionado,
-        x="Registro_temporal",
-        y=["Temperatura_Celsius", "Humedad_relativa[%]"],
-        color=df_seleccionado.index.get_level_values(0),
-    )
-    # Mostrar gráfico en Streamlit
+
+    # Line chart for temperature and humidity over time, grouped by location
+    fig3 = px.line(df_seleccionado, x="Registro_temporal", y=["Temperatura_Celsius", "Humedad_relativa[%]"], color=df_seleccionado.index.get_level_values(0))
     st.plotly_chart(fig3)
+
 else:
     st.warning("Por favor, selecciona al menos una ubicación.")
-
