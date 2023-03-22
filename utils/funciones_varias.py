4a6bc7aa81ef0a0da54fc6de2e9095aeeb36871c
Enrique Vasallo Fernández <65018705+Vasallo94@users.noreply.github.com>
2023-03-22 23:21:45 +0100

    prueba

--- utils/funciones_varias.py
+++ utils/funciones_varias.py
@@ -25,6 +25,7 @@ def load_data():
     df_habitacion['ubicacion'] = 'Habitación'
     df_salon['ubicacion'] = 'Salón'
     df = pd.merge(df_habitacion, df_salon, how='outer')
+    df = df.index.name = "Ubicación"
     return df
 
 def create_sidebar(ubicaciones_disponibles, opciones_sampling):
@@ -32,10 +33,17 @@ def create_sidebar(ubicaciones_disponibles, opciones_sampling):
     sampling_predeterminado = st.sidebar.selectbox('Selecciona el muestreo de tiempo', opciones_sampling, index=0)
     return ubicaciones_predeterminadas, sampling_predeterminado
 
+# def filter_data(df, ubicaciones_predeterminadas, sampling_predeterminado):
+#     df_seleccionado = df.loc[df.index.get_level_values("ubicacion").isin(ubicaciones_predeterminadas)]
+#     #df_seleccionado = df_seleccionado.reset_index(level=1)
+#     #df_seleccionado["Ubicación"] = df_seleccionado["Ubicación"].apply(lambda x: x[1])
+#     df_seleccionado["Registro_temporal"] = pd.to_datetime(df_seleccionado["Registro_temporal"])
+#     df_seleccionado = df_seleccionado.set_index("Registro_temporal")
+#     df_seleccionado = df_seleccionado.resample(sampling_predeterminado).mean()
+#     return df_seleccionado
+
 def filter_data(df, ubicaciones_predeterminadas, sampling_predeterminado):
-    df_seleccionado = df.loc[df.index.get_level_values("ubicacion").isin(ubicaciones_predeterminadas)]
-    #df_seleccionado = df_seleccionado.reset_index(level=1)
-    #df_seleccionado["Ubicación"] = df_seleccionado["Ubicación"].apply(lambda x: x[1])
+    df_seleccionado = df.loc[df.index.get_level_values("Ubicación").isin(ubicaciones_predeterminadas)]
     df_seleccionado["Registro_temporal"] = pd.to_datetime(df_seleccionado["Registro_temporal"])
     df_seleccionado = df_seleccionado.set_index("Registro_temporal")
     df_seleccionado = df_seleccionado.resample(sampling_predeterminado).mean()
@@ -44,9 +52,9 @@ def filter_data(df, ubicaciones_predeterminadas, sampling_predeterminado):
 def create_plotly_charts(df_seleccionado):
     # Line chart for temperature over time
     figs = []
-    fig1 = px.line(df_seleccionado, x=df_seleccionado.index, y="Temperatura_Celsius", color="ubicacion",
+    fig1 = px.line(df_seleccionado, x=df_seleccionado.index, y="Temperatura_Celsius", color="Ubicación",
                    color_discrete_map={"Habitación": "#3DDEE0", "Salón": "#E07B3D"},
-                   labels={"Temperatura_Celsius": "Temperatura (Celsius)", "Registro_temporal": "Tiempo", "ubicacion": "Ubicación"})
+                   labels={"Temperatura_Celsius": "Temperatura (Celsius)", "Registro_temporal": "Tiempo", "Ubicación": "Ubicación"})
     fig1.update_layout(title="Temperatura", yaxis_title="Temperatura (Celsius)")
     figs.append(fig1)
 
