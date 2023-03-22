ad0f06b13ebabdaba5357f9e130738d2a2bac502
Enrique Vasallo Fernández <65018705+Vasallo94@users.noreply.github.com>
2023-03-23 00:08:51 +0100

    d

--- utils/funciones_varias.py
+++ utils/funciones_varias.py
@@ -42,19 +42,19 @@ def create_sidebar(ubicaciones_disponibles, opciones_sampling):
 #     df_seleccionado = df_seleccionado.resample(sampling_predeterminado).mean()
 #     return df_seleccionado
 
-# def filter_data(df, ubicaciones_predeterminadas, sampling_predeterminado):
-#     df_seleccionado = df.loc[df["Ubicación"].isin(ubicaciones_predeterminadas)]
-#     df_seleccionado.loc[:, "Registro_temporal"] = pd.to_datetime(df_seleccionado["Registro_temporal"])
-#     df_seleccionado = df_seleccionado.set_index("Registro_temporal")
-#     #df_seleccionado = df_seleccionado.resample(sampling_predeterminado).mean()
-#     df_seleccionado = df_seleccionado.resample(sampling_predeterminado).mean(numeric_only=True)
-#     return df_seleccionado
-
 def filter_data(df, ubicaciones_predeterminadas, sampling_predeterminado):
-    df_seleccionado = df[df["Ubicación"].isin(ubicaciones_predeterminadas)].copy()
-    df_seleccionado.set_index('datetime', inplace=True)
-    df_seleccionado = df_seleccionado.groupby(pd.Grouper(freq=sampling_predeterminado)).mean()
+    df_seleccionado = df.loc[df["Ubicación"].isin(ubicaciones_predeterminadas)]
+    df_seleccionado.loc[:, "Registro_temporal"] = pd.to_datetime(df_seleccionado["Registro_temporal"])
+    df_seleccionado = df_seleccionado.set_index("Registro_temporal")
+    #df_seleccionado = df_seleccionado.resample(sampling_predeterminado).mean()
+    df_seleccionado = df_seleccionado.resample(sampling_predeterminado).mean(numeric_only=True)
     return df_seleccionado
+
+# def filter_data(df, ubicaciones_predeterminadas, sampling_predeterminado):
+#     df_seleccionado = df[df["Ubicación"].isin(ubicaciones_predeterminadas)].copy()
+#     df_seleccionado.set_index('datetime', inplace=True)
+#     df_seleccionado = df_seleccionado.groupby(pd.Grouper(freq=sampling_predeterminado)).mean()
+#     return df_seleccionado
 
 def create_plotly_charts(df_seleccionado):
     # Line chart for temperature over time
