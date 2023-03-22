700032837cfa318f9621814000cce0de705fbb3b
Enrique Vasallo Fernández <65018705+Vasallo94@users.noreply.github.com>
2023-03-22 22:07:32 +0100

    Update funciones_varias.py

--- utils/funciones_varias.py
+++ utils/funciones_varias.py
@@ -20,10 +20,9 @@ def create_chart(df, x, y, chart_type, title, yaxis_title, xaxis_title=None, hei
     st.plotly_chart(fig, use_container_width=True, height=height)
 
 def load_data():
-    df_habitacion = pd.read_csv("data/Habitacion_export_202303182309.csv")
-    df_salon = pd.read_csv("data/Salon_export_202303182309.csv")
-    df = pd.concat([df_habitacion, df_salon], keys=["Habitación", "Salón"], names=["Ubicación"])
-    df = df.reorder_levels([1, 0]).sort_index()
+    df = load_dataframes()
+    df["Registro_temporal"] = pd.to_datetime(df["Registro_temporal"])
+    df = df.set_index(["Ubicación", "Registro_temporal"])
     return df
 
 def create_sidebar(ubicaciones_disponibles, opciones_sampling):
@@ -32,28 +31,21 @@ def create_sidebar(ubicaciones_disponibles, opciones_sampling):
     return ubicaciones_predeterminadas, sampling_predeterminado
 
 def filter_data(df, ubicaciones_predeterminadas, sampling_predeterminado):
-    df_seleccionado = df.loc[df.index.get_level_values("Ubicación").isin(ubicaciones_predeterminadas)]
-    df_seleccionado = df_seleccionado.reset_index(level=1)
-    df_seleccionado["Ubicación"] = df_seleccionado["Ubicación"].apply(lambda x: x[1])
-    df_seleccionado["Registro_temporal"] = pd.to_datetime(df_seleccionado["Registro_temporal"])
-    df_seleccionado = df_seleccionado.set_index("Registro_temporal")
+    df_seleccionado = df.loc[ubicaciones_predeterminadas]
     df_seleccionado = df_seleccionado.resample(sampling_predeterminado).mean()
     return df_seleccionado
 
 def create_plotly_charts(df_seleccionado):
     # Line chart for temperature over time
-    fig1 = px.line(df_seleccionado, x=df_seleccionado.index, y="Temperatura_Celsius", color="Ubicación",
+    fig1 = px.line(df_seleccionado, x=df_seleccionado.index.get_level_values("Registro_temporal"), y="Temperatura_Celsius", color="Ubicación",
                    color_discrete_map={"Habitación": "#3DDEE0", "Salón": "#E07B3D"},
                    labels={"Temperatura_Celsius": "Temperatura (Celsius)", "Registro_temporal": "Tiempo", "Ubicación": "Ubicación"})
     fig1.update_layout(title="Temperatura", yaxis_title="Temperatura (Celsius)")
-    figs.append(fig1)
-
+    
     # Line chart for humidity over time
-    fig2 = px.line(df_seleccionado, x=df_seleccionado.index, y="Humedad_relativa[%]", color="Ubicación",
+    fig2 = px.line(df_seleccionado, x=df_seleccionado.index.get_level_values("Registro_temporal"), y="Humedad_relativa[%]", color="Ubicación",
                    color_discrete_map={"Habitación": "#3DDEE0", "Salón": "#E07B3D"},
                    labels={"Humedad_relativa[%]": "Humedad relativa (%)", "Registro_temporal": "Tiempo", "Ubicación": "Ubicación"})
     fig2.update_layout(title="Humedad", yaxis_title="Humedad relativa (%)")
-    figs.append(fig2)
-    return figs
-
-
+    
+    return [fig1, fig2]
