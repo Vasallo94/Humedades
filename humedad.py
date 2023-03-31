6a6b68e26ca67e03c3f59f038383525668dde8b7
Vasallo94 <65018705+Vasallo94@users.noreply.github.com>
2023-04-01 00:47:37 +0200

    box

--- humedad.py
+++ humedad.py
@@ -7,7 +7,8 @@ st.set_page_config(page_title="Humedades", layout="wide", page_icon="💦")
 st.set_option("deprecation.showPyplotGlobalUse", False)
 
 df = load_dataframes()
-ubicaciones_predeterminadas = st.sidebar.multiselect("Selecciona la ubicación", ["Habitación", "Salón"], default=["Habitación", "Salón"])
+ubicaciones_predeterminadas = st.sidebar.multiselect(
+    "Selecciona la ubicación", ["Habitación", "Salón"], default=["Habitación", "Salón"])
 df_seleccionado = df[df["ubicacion"].isin(ubicaciones_predeterminadas)]
 
 if df_seleccionado.empty:
@@ -18,10 +19,13 @@ else:
         "color": "ubicacion",
         "color_discrete_map": {"Habitación": "#3DDEE0", "Salón": "#E07B3D"},
     }
-    create_chart(df_seleccionado, "Registro_temporal", "Temperatura_Celsius", px.line, "Temperatura", "Temperatura (Celsius)", **chart_config)
-    create_chart(df_seleccionado, "Registro_temporal", "Humedad_relativa[%]", px.line, "Humedad", "Humedad relativa (%)", **chart_config)
-    create_chart(df_seleccionado, "Registro_temporal", ["Humedad_relativa[%]", "Temperatura_Celsius"], px.line, "Humedad y temperatura", "Humedad y temperatura", **chart_config)
-    create_chart(df_seleccionado, "Humedad_relativa[%]", "Temperatura_Celsius", px.scatter, "Humedad y temperatura (dispersión)", "Temperatura (Celsius)", "Humedad relativa", marginal_y="histogram", marginal_x="histogram", trendline="ols", trendline_color_override="darkseagreen", **chart_config)
-    create_chart(df_seleccionado, "Registro_temporal", ["Humedad_relativa[%]", "Temperatura_Celsius"], px.line, "Humedad y temperatura por ubicación", "Humedad y temperatura", "Tiempo", height=700, facet_col="ubicacion", **chart_config)
-    create_chart(df_seleccionado, "Registro_temporal", ["Humedad_relativa[%]", "Temperatura_Celsius"], px.box, "Humedad y temperatura por ubicación", "Humedad y temperatura", "Tiempo", height=700, facet_col="ubicacion", **chart_config)
-
+    create_chart(df_seleccionado, "Registro_temporal", "Temperatura_Celsius",
+                 px.line, "Temperatura", "Temperatura (Celsius)", **chart_config)
+    create_chart(df_seleccionado, "Registro_temporal",
+                 "Humedad_relativa[%]", px.line, "Humedad", "Humedad relativa (%)", **chart_config)
+    create_chart(df_seleccionado, "Registro_temporal", [
+                 "Humedad_relativa[%]", "Temperatura_Celsius"], px.line, "Humedad y temperatura", "Humedad y temperatura", **chart_config)
+    create_chart(df_seleccionado, "Humedad_relativa[%]", "Temperatura_Celsius", px.scatter, "Humedad y temperatura (dispersión)", "Temperatura (Celsius)",
+                 "Humedad relativa", marginal_y="histogram", marginal_x="box", trendline="ols", trendline_color_override="darkseagreen", **chart_config)
+    create_chart(df_seleccionado, "Registro_temporal", ["Humedad_relativa[%]", "Temperatura_Celsius"], px.line,
+                 "Humedad y temperatura por ubicación", "Humedad y temperatura", "Tiempo", height=700, facet_col="ubicacion", **chart_config)
